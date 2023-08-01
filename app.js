108cafa85798ddd5872389a011368aefc1c11a4d
kecheste <aben.ezer443@gmail.com>
2023-08-01 11:38:02 +0300

    Additional fixes

--- app.js
+++ app.js
@@ -145,6 +145,8 @@ app.use((err, req, res, next) => {
   res.status(status).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Serving on ${port}`);
 });
